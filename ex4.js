758a18e1853f187adc5e664f05d9a1d67f5bb774
Victor <victoraweb@gmail.com>
2016-03-20 10:56:01 -0300

    Ex4 - Local

---
+++ ex4.js
@@ -0,0 +1,32 @@
+// GOOD OLD FORM
+
+// Modules
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Definitions
+var app = express();
+var port = 3000;
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// View engine
+app.set('view engine', 'jade');
+
+// Path to static files
+app.set('views', './');
+
+// Routes
+app.get('/form', function(req, res) {
+    res.render('ex4Form');
+});
+
+app.post('/form', function(req, res) {
+    res.send(req.body.str.split('').reverse().join(''));
+});
+
+app.listen(port, function() {
+    console.log('Server running at http://localhost:' + port);
+});
