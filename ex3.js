23a2cae020e53936f06a3e5e25594b89832d11a6
Victor <victoraweb@gmail.com>
2016-03-20 10:11:48 -0300

    Ex3 - Nodeschool

--- ex3.js
+++ ex3.js
@@ -2,10 +2,10 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = 3000;
+var port = process.argv[2];
 
 // Specify the path to the template files in the folder templates
-app.set('views', './');
+app.set('views', path.join(__dirname, 'templates'));
 
 // Tell Express what template engine to use
 app.set('view engine', 'jade');
@@ -13,9 +13,7 @@ app.set('view engine', 'jade');
 
 app.get('/home', function(req, res) {
     // Accepts a template name and data (called locals)
-    res.render('ex6View', { date: new Date().toDateString() });
+    res.render(process.argv[3], { date: new Date().toDateString() });
 });
 
-app.listen(port, function() {
-    console.log('Server running at: http://localhost:' + port);
-});
+app.listen(port);
